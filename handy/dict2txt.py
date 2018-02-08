80d053469e2a59b30e906d1e2197382fc7e1170b
gustavkkk <gustav0125@outlook.com>
2018-02-08 15:28:29 +0800

    add-dict2json

--- handy/dict2txt.py
+++ handy/dict2txt.py
@@ -7,16 +7,22 @@ Created on Thu Feb  1 05:00:14 2018
 
 import json
 
-def dict2txt(dict_,filename):
+def dict2json(dict_,filename):
     with open(filename, 'w') as file:
         string = json.dumps(dict_).replace(",", ",\n")
         file.write(string)
-
-def txt2dict(filepath):
+       
+def json2dict(filepath):
     with open(filepath, 'r') as file:
         dictdump = json.loads(file.read())
         return dictdump
     return None
+
+def dict2txt(dict_,filename):
+    dict2json(dict_,filename)
+    
+def txt2dict(filepath):
+    json2dict(filepath)
     
 if __name__ == "__main__":
     dict2txt({'a':1,'b':2},'test.json')
