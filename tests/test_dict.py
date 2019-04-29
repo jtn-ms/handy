5ee314194163b789a383918f799870078f2b0a7a
bluesky0125 <time2go@yeah.net>
2019-04-29 14:40:22 +0800

    refine

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -1,4 +1,4 @@
-from handy.dict.mixedict import delkey,isin
+from handy.dict.puredict import delkey,isin
 
 def test_delkey():
     testdata = {1:111,2:222,3:{1:1111,2:2222},4:{1:1,2:2,3:3,4:4,5:{1:11}}}
