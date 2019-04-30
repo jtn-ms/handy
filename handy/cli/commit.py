b9561d265a88edc1a7086bf56972a4ce2cd6afdd
bluesky0125 <time2go@yeah.net>
2019-04-30 10:49:08 +0800

    change

--- handy/cli/commit.py
+++ handy/cli/commit.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-msg_help = ""
+msg_help = "Written by junying, 2019-04-30 \
+            \nUsage: commit"
 msg_comment = "enter comment: "
 
 cmd_add = "git add ."
@@ -9,8 +10,8 @@ cmd_comment = "git commit -m %s"
 cmd_push = "git push origin master"
 
 def main():
-    os.system(cmd_add)
     if sys.version_info[0] == 2: comment = raw_input(msg_comment)
     else: comment = input(msg_comment)
+    os.system(cmd_add)
     os.system(cmd_comment%comment)
     os.system(cmd_push)
