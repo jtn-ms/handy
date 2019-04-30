545a897e10f7bb3845f3afffd4202449989c66ed
bluesky0125 <time2go@yeah.net>
2019-04-30 16:45:27 +0800

    refine

--- handy/cli/transfer.py
+++ handy/cli/transfer.py
@@ -2,14 +2,14 @@ import os
 import sys
 
 msg_help_upload = "Written by junying, 2019-04-30 \
-                   \nUsage: upload"
+                   \nUsage: upload [filename]"
 msg_not_found = "file doesn't exist."
             
 def upload():
     if len(sys.argv) < 2: print(msg_help_upload); return
     if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]): print(msg_not_found); return
     os.system("curl --progress-bar --upload-file {0} https://transfer.sh/{1} | tee /dev/null".\
-                                    format(sys.argv[1],os.path.split(sys.argv[1])))
+                                    format(sys.argv[1],os.path.split(sys.argv[1])[1]))
 
 msg_help_download = "Written by junying, 2019-04-30 \
                     \nUsage: download [filename] [password]"
