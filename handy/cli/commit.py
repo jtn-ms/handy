037ff3c0c5363e34208b2e5337f231fa6fe90483
bluesky0125 <time2go@yeah.net>
2019-04-30 10:59:24 +0800

    refine

--- handy/cli/commit.py
+++ handy/cli/commit.py
@@ -4,12 +4,15 @@ import sys
 msg_help = "Written by junying, 2019-04-30 \
             \nUsage: commit"
 msg_comment = "enter comment: "
+msg_not_found = "git repo not found"
 
 cmd_add = "git add ."
 cmd_comment = "git commit -m %s"
 cmd_push = "git push origin master"
 
 def main():
+    if len(sys.argv) > 1: print(msg_help); return
+    if '.git' not in os.listdir('.'): print(msg_not_found); print(msg_help); return
     if sys.version_info[0] == 2: comment = raw_input(msg_comment)
     else: comment = input(msg_comment)
     os.system(cmd_add)
