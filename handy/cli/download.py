a4ae17530e419d94fc503a8b310bbdea96371719
bluesky0125 <time2go@yeah.net>
2019-05-06 17:06:46 +0800

    refine

---
+++ handy/cli/download.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+msg_help_upload = "Written by junying, 2019-05-06 \
+                   \nUsage: download [filename]"
+
+from .constants import msg_file_not_found
+
+def download():
+    if len(sys.argv) < 2: print(msg_help_upload); return
+    if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]): print(msg_file_not_found); return
+    with open(sys.argv[1]) as file:
+        for line in file:
+            os.system('wget  %s'%line)
--- handy/cli/transfer.py
+++ handy/cli/transfer.py
@@ -11,6 +11,7 @@ def upload():
     if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]): print(msg_file_not_found); return
     os.system("curl --progress-bar --upload-file {0} https://transfer.sh/{1} | tee /dev/null".\
                                     format(sys.argv[1],os.path.split(sys.argv[1])[1]))
+    os.system('echo')
 
 msg_help_download = "Written by junying, 2019-04-30 \
                     \nUsage: download [filename] [password]"
