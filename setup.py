ab67d44635de99a834518b9a71576b8129878481
bluesky0125 <time2go@yeah.net>
2019-05-27 10:11:19 +0800

    version mistake resolved, version up

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from io import open
 from setuptools import find_packages, setup
 
 with open('handy/_version.py', 'r') as f:
+    version = '0.0.1'
     for line in f:
         if line.startswith('__version__'): version = line.strip().split('=')[1].strip(' \'"'); break
-    else: version = '0.0.1'
-
+    
 with open('README.rst', 'r', encoding='utf-8') as f:
     readme = f.read()
 
