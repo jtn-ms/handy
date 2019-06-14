9df97777c5e0260564a0aa4e1f1f4e02c4cfd2fb
bluesky0125 <time2go@yeah.net>
2019-06-14 16:25:57 +0800

    transfer.sh + yandex up and down

--- handy/cli/transfer.py
+++ handy/cli/transfer.py
@@ -1,22 +1,35 @@
 import os
 import sys
-
+# curl --progress-bar --upload-file {0} https://transfer.sh/{1} | tee /dev/null
+# wget https://transfer.sh/{1}/{0} | tee /dev/null
 msg_help_upload = "Written by junying, 2019-04-30 \
-                   \nUsage: upload [filename]"
+                   \nUsage: upload [filename] \
+                   \nUsage: upload [filename] --print\
+                   \nUsage: upload [filename] --mail"
 
 from ._constants import msg_file_not_found
-
+import subprocess
+from handy.hack.mail import send_mail
 def upload():
-    if len(sys.argv) < 2: print(msg_help_upload); return
-    if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]): print(msg_file_not_found); return
-    os.system("curl --progress-bar --upload-file {0} https://transfer.sh/{1} | tee /dev/null".\
-                                    format(sys.argv[1],os.path.split(sys.argv[1])[1]))
-    os.system('echo')
+    if len(sys.argv) < 2 or any(argv.startswith("-h") for argv in sys.argv): return msg_help_upload
+    if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]): return msg_file_not_found
+    if any(argv.startswith('--mail') for argv in sys.argv): send_mail(files=[sys.argv[1]],subject="handi upload")
+    else:
+        result = subprocess.check_output("curl --silent --progress-bar --upload-file {0} https://transfer.sh/{1}".\
+                                        format(sys.argv[1],os.path.split(sys.argv[1])[1]),shell=True)
+        if any(argv.startswith('--print') for argv in sys.argv): print(result)
+        send_mail(subject=result)
 
 msg_help_download = "Written by junying, 2019-04-30 \
-                    \nUsage: download [filename] [password]"
-                
+                    \nUsage:  download \
+                    \nUsage2: download -h"
+
+from handy.hack.mail import read_email
 def download():
-    if len(sys.argv) < 3: print(msg_help_download); return
-    os.system("wget https://transfer.sh/{1}/{0} | tee /dev/null".format(sys.argv[1],sys.argv[2]))
+    if any("-h" in argv for argv in sys.argv): return msg_help_download
+    when,who,what=read_email()
+    if 'olzs' not in who: return "no uploaded at present!"
+    if 'handi upload' in what: return "please check mail directly."
+    if 'https://transfer.sh' not in what: return "wrong message: the recent message not from transfer.sh"
+    os.system("wget --quiet {0} | tee /dev/null".format(what))
     
---
+++ handy/hack/mail.py
@@ -0,0 +1,72 @@
+import imaplib
+import email
+
+import smtplib, ssl
+import datetime
+
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+def send_mail(sender='secretolzs@yandex.com', passwd="Aireoqkwkrolzs",
+              receivers=['delivery.olzs@yandex.com'], files=None,subject="Report",
+              server='smtp.yandex.com',port=587):
+    assert(isinstance(receivers, list))
+
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = COMMASPACE.join(receivers)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText("this report is classified."))
+    # patching
+    for f in files or []:
+        with open(f, "rb") as file:
+            part = MIMEApplication(
+                file.read(),
+                Name=basename(f) 
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+    # sending
+    try:
+        smtp = smtplib.SMTP(server,port)
+        smtp.starttls()
+        smtp.login(sender, passwd)
+        smtp.sendmail(sender, receivers, msg.as_string())
+        smtp.close()
+    except Exception as e:
+        return False
+    return True
+
+def read_email(server_domain="imap.yandex.com",\
+               usrname="delivery.olzs@yandex.com",\
+               password="lqqEo&zb"):
+    try:
+        mail = imaplib.IMAP4_SSL(server_domain)
+        mail.login(usrname, password)
+        mail.list()
+        # Out: list of "folders" aka labels in gmail.
+        mail.select("inbox") # connect to inbox.
+        result, data = mail.search(None, "ALL")
+
+        ids = data[0] # data is a list.
+        id_list = ids.split() # ids is a space separated string
+        latest_email_id = id_list[-1] # get the latest
+
+        # fetch the email body (RFC822) for the given ID
+        result, data = mail.fetch(latest_email_id, "(RFC822)") 
+
+        raw_email = data[0][1] # here's the body, which is raw text of the whole email
+        # including headers and alternate payloads
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                msg = email.message_from_string(response_part[1])
+                return len(id_list),msg['from'],msg['subject']
+    except Exception, e:
+        pass
+    return None,None,None
