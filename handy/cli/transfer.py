2c31448bc0093e71c03c49e301d7f3bcef20f9ba
bluesky0125 <time2go@yeah.net>
2019-06-15 17:18:43 +0800

    veil mail addresses

--- handy/cli/transfer.py
+++ handy/cli/transfer.py
@@ -13,12 +13,13 @@ from handy.hack.mail import send_mail
 def upload():
     if len(sys.argv) < 2 or any(argv.startswith("-h") for argv in sys.argv): return msg_help_upload
     if not os.path.exists(sys.argv[1]) or not os.path.isfile(sys.argv[1]): return msg_file_not_found
-    if any(argv.startswith('--mail') for argv in sys.argv): send_mail(files=[sys.argv[1]],subject=sys.argv[1])
+    usedefault = True if any(argv.startswith('--default') for argv in sys.argv) else False
+    if any(argv.startswith('--mail') for argv in sys.argv): send_mail(files=[sys.argv[1]],subject=sys.argv[1],smode=usedefault)
     else:
         result = subprocess.check_output("curl --silent --progress-bar --upload-file {0} https://transfer.sh/{1}".\
                                         format(sys.argv[1],os.path.split(sys.argv[1])[1]),shell=True)
         if any(argv.startswith('--print') for argv in sys.argv): print(result)
-        send_mail(subject=result)
+        send_mail(subject=result,smode=usedefault)
 
 msg_help_download = "Written by junying, 2019-04-30 \
                     \nUsage:  download \
@@ -27,9 +28,10 @@ msg_help_download = "Written by junying, 2019-04-30 \
 from handy.hack.mail import read_email
 def download():
     if any("-h" in argv for argv in sys.argv): return msg_help_download
-    when,who,what=read_email()
+    usedefault = True if any(argv.startswith('--default') for argv in sys.argv) else False
+    when,who,what=read_email(smode=usedefault)
     if not when or not who or not what: return "network connection not good."
     if 'olzs' not in who: return "no uploaded at present!"
-    if 'https://transfer.sh' not in what: return
+    if 'https://transfer.sh' not in what: return 
     os.system("wget --quiet {0} | tee /dev/null".format(what))
     
--- handy/hack/mail.py
+++ handy/hack/mail.py
@@ -10,11 +10,19 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 
+import os,sys
+import getpass
 def send_mail(sender='secretolzs@yandex.com', passwd="Aireoqkwkrolzs",
               receivers=['delivery.olzs@yandex.com'], files=None,subject="Report",
-              server='smtp.yandex.com',port=587):
-    assert(isinstance(receivers, list))
-
+              server='smtp.yandex.com',port=587,smode=False):
+    if os.path.exists('/tmp/mail.outbox'):
+        with open('/tmp/mail.outbox','r') as file:
+            sender,passwd,receivers = [line for line in file]
+    elif not smode:
+        sender = raw_input("Sender: ") if sys.version_info[0] == 2 else input("Sender:")
+        passwd = getpass.getpass()
+        receivers = raw_input("Receivers: ") if sys.version_info[0] == 2 else input("Receivers:")
+    if isinstance(receivers,str) and "," in receivers:receivers= receivers.split(",")
     msg = MIMEMultipart()
     msg['From'] = sender
     msg['To'] = COMMASPACE.join(receivers)
@@ -39,13 +47,26 @@ def send_mail(sender='secretolzs@yandex.com', passwd="Aireoqkwkrolzs",
         smtp.login(sender, passwd)
         smtp.sendmail(sender, receivers, msg.as_string())
         smtp.close()
+        print("Successfully Uploaded!")
+        # saving incredentials
+        with open('/tmp/mail.outbox','w') as file:
+            file.writelines(['%s\n'%sender,
+                             '%s\n'%passwd,
+                             '%s\n'%(','.join(receivers))])
     except Exception as e:
         return False
     return True
 
 def read_email(server_domain="imap.yandex.com",\
                usrname="delivery.olzs@yandex.com",\
-               password="lqqEo&zb"):
+               password="lqqEo&zb",smode=False):
+           
+    if os.path.exists('/tmp/mail.inbox'):
+        with open('/tmp/mail.inbox','r') as file:
+            usrname,password = [line for line in file]
+    elif not smode:
+        usrname = raw_input("Receiver: ") if sys.version_info[0] == 2 else input("Receiver:")
+        password = getpass.getpass()
     try:
         mail = imaplib.IMAP4_SSL(server_domain)
         mail.login(usrname, password)
@@ -70,6 +91,10 @@ def read_email(server_domain="imap.yandex.com",\
                     attachment = msg.get_payload()[1]
                     with open(msg['subject'], 'wb') as file:
                         file.write(attachment.get_payload(decode=True))
+                    print(" Downloaded %s Successfully!"%msg['subject'])
+                with open('/tmp/mail.inbox','w') as file:
+                    file.writelines(['%s\n'%usrname,
+                                    '%s\n'%password])
                 return len(id_list),msg['from'],msg['subject']
     except Exception as e: pass
     return None,None,None
