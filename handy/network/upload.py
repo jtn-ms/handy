386daa762fe4f5887e25fddcfd2c03c2cdaa91c5
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-20 05:09:15 +0300

    Removal of master/slave phrasing

--- handy/network/upload.py
+++ handy/network/upload.py
@@ -30,7 +30,7 @@ def upload_repo():
     comment = input('Type Comment:\n')
     os.system('git add .')
     os.system('git commit -m %s'%comment)
-    os.system('git push origin master')
+    os.system('git push origin main')
     
 def main():
     pass
